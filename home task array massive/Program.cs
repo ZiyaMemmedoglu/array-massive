46f1d86f8ddbe645c2f37a5f3e0df899897e41a2
ZiyaMemmedoglu <ziyamm@code.edu.az>
2024-10-22 12:10:48 +0400

    Array Massive

---
+++ home task array massive/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+#region
+
+//homm task1.Verilmiş Arrayın elementlərinin cəmini ekrana çap edin
+
+//int[] numbers2 = { 10, 22, 35, 48, 50, };
+//int sum = 0;
+//for (int i = 0; i < numbers2.Length; i++)
+//{
+//    sum += numbers2[i];
+//}
+//Console.WriteLine(sum);
+
+#endregion
+
+#region
+//homm task 2. Verilmiş int Arrayın ən böyük dəyərə sahib elementini tapın
+
+//int[] number = { 1, 2, 3, 4, 61, 60, 10 };
+//int max = number[default];
+//for (int i = 0; i < number.Length; i++)
+//    if (number[i] > max)
+
+//        max = number[i];
+//Console.WriteLine(max);
+
+#endregion
+
+
+
+#region
+//task 3.Verilmiş Arrayın ilk və son elementlərinin cəmini tapın
+
+//int[] number = { 5, 2, 3, 4, 5, 6, 15 };
+//int sum = default;
+//for (int i = 0; i < number.Length; i++)
+//    if (number[i] == 0)
+//        break;
+//{
+//    Console.WriteLine(number[0]);
+//}
+//for (int i = 0; i < number.Length; i++)
+//    if (number[i] == 0)
+//        break;
+//Console.WriteLine(number[number.Length - 1]);
+
+//for (int i = 0; i < number.Length; i++) ;
+//sum = number[0] + (number[number.Length - 1]);
+//Console.WriteLine(sum);
+
+
+
+#endregion
